11e90b7a2e6bebf22f7d96396e970d394efae241
Стас Асташенко <stasastasenko@192.168.1.54>
2022-02-12 16:42:19 +0300

    добавил файлы

---
+++ slide.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll('.slide');
+
+for(let slide of slides){
+    slide.addEventListener('click', () => {
+        removeActive()
+
+        slide.classList.add('active');
+    })
+};
+
+function removeActive (){
+    for(let slide of slides){
+        slide.classList.remove('active');
+    }
+}
